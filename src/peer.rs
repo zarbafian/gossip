a00c6277114493cfc881efbfc5f28299281dd435
pouriya-zarbafian <pouriya.zarbafian@example.com>
2020-12-22 16:14:42 +0100

    remove unused methods

--- src/peer.rs
+++ src/peer.rs
@@ -1,9 +1,5 @@
 use std::hash::{Hash, Hasher};
-use std::error::Error;
 use serde::{Serialize, Deserialize};
-// TODO: Refactor
-// Byte separator between the peer address and the peer age
-const SEPARATOR: u8 = 0x2C; // b','
 
 /// Information about a peer
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -39,49 +35,6 @@ impl Peer {
     /// Returns the address of peer
     pub fn address(&self) -> &str { &self.address }
 
-    /// Serializes peer into an array of bytes.
-    /// Starts with the address of the peer first followed by the age of the peer
-    /// address and age are separated by a [SEPARATOR] byte.
-    pub fn as_bytes(&self) -> Vec<u8> {
-        // peer address
-        let mut v = self.address.as_bytes().to_vec();
-        // separator
-        v.push(SEPARATOR);
-        // peer age: first byte
-        v.push((self.age >> 8) as u8);
-        // peer age: second byte
-        v.push((self.age & 0x00FF) as u8);
-        v
-    }
-
-    /// Deserializes a peer from an array of bytes
-    ///
-    /// # Arguments
-    ///
-    /// * `bytes` - A peer serialized as bytes
-    pub fn from_bytes(bytes: &[u8]) -> Result<Peer, Box<dyn Error>> {
-        // retrieve index of separator
-        let separator_index = bytes.iter().enumerate()
-            .find(|(_, b)| { **b == SEPARATOR})
-            .map(|(i, _)| {i});
-        if let Some(index) = separator_index {
-            // check that there are exactly two bytes for the age after separator
-            if bytes.len() != index + 3 {
-                Err("invalid age")?
-            }
-            // retrieve address
-            let address = String::from_utf8(bytes[..index].to_vec())?;
-            // build age
-            let age = ((bytes[index+1] as u16) << 8 ) + (bytes[index+2] as u16);
-            Ok(Peer{
-                address,
-                age,
-            })
-        }
-        else {
-            Err("peer separator not found")?
-        }
-    }
 }
 impl Eq for Peer {}
 impl PartialEq for Peer {
